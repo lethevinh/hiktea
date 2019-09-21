64ade977ab3ec835b4980f78db54d3fbac5904d5
levinh <lethevinh.qng@gmail.com>
2019-09-21 21:28:10 +0700

    change style page content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -238,144 +238,17 @@
         <div class="row justify-content-center testimonial_slider testimonial_style1 carousel_slide3 owl-carousel owl-theme" data-margin="30" data-loop="true" data-autoplay="true" data-dots="true">
             @foreach($branchs as $branch)
             <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.04s">
-                <div class="blog_img"><a href="#">
-                        <img src="assets/images/blog_small_img1.jpg" alt="blog_small_img1">
+                <div class="blog_img">
+                    <a href="#">
+                        <img src="{{$branch->images[0]}}" alt="blog_small_img1">
                     </a>
-                    <div class="blog_date style1">
-                        <h4>02</h4><span>May</span>
-                    </div>
                 </div>
                 <div class="blog_content">
                     <h6 class="blog_title"><a href="#">{{$branch->title}}</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>4 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p>
-                    <a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
+                    <a class="blog_link" href="#">Xem Chi tiết<i class="ion-ios-arrow-right"></i></a>
                 </div>
             </div>
             @endforeach
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.05s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img2.jpg" alt="blog_small_img2"></a>
-                    <div class="blog_date style1">
-                        <h4>25</h4><span>Mar</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>3 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.06s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img3.jpg" alt="blog_small_img3"></a>
-                    <div class="blog_date style1">
-                        <h4>08</h4><span>Aug</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>5 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.04s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img1.jpg" alt="blog_small_img1"></a>
-                    <div class="blog_date style1">
-                        <h4>02</h4><span>May</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>4 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.05s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img2.jpg" alt="blog_small_img2"></a>
-                    <div class="blog_date style1">
-                        <h4>25</h4><span>Mar</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>3 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.06s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img3.jpg" alt="blog_small_img3"></a>
-                    <div class="blog_date style1">
-                        <h4>08</h4><span>Aug</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>5 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.04s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img1.jpg" alt="blog_small_img1"></a>
-                    <div class="blog_date style1">
-                        <h4>02</h4><span>May</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>4 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.05s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img2.jpg" alt="blog_small_img2"></a>
-                    <div class="blog_date style1">
-                        <h4>25</h4><span>Mar</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>3 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.06s">
-                <div class="blog_img"><a href="#"><img src="assets/images/blog_small_img3.jpg" alt="blog_small_img3"></a>
-                    <div class="blog_date style1">
-                        <h4>08</h4><span>Aug</span>
-                    </div>
-                </div>
-                <div class="blog_content">
-                    <h6 class="blog_title"><a href="#">Varius Phasellus blandit massa enim</a></h6>
-                    <ul class="list_none blog_meta">
-                        <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>
-                        <li><a href="#"><i class="far fa-comments"></i>5 Comment</a></li>
-                    </ul>
-                    <p>Phasellus blandit massa enim elit variununc Lorems ipsum dolor sit consectetur industry. If you are use passage of Lorem Ipsum.</p><a class="blog_link" href="#">Read More <i class="ion-ios-arrow-right"></i></a>
-                </div>
-            </div>
         </div>
     </div>
     <div class="overlap_shape">
